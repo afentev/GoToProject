e27fc3ceddf4dd1d16f5c82f030b6f3379baee66
afentev <kirill.afentyev@yandex.ru>
2017-08-22 00:45:17 +0300

    added keyboard; all requests are fixed

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import telebot
-
-bot = telebot.TeleBot('335976113:AAG0Z4lhYsLWRZ7KUsvTKRpP2SacaytTn9M')
+token = '335976113:AAG0Z4lhYsLWRZ7KUsvTKRpP2SacaytTn9M'
+bot = telebot.TeleBot(token)
 id_ = -216295082  # заменить на id беседы, где бот должен работать
-users = set()
+users = []
+tasks = []
 
 
 @bot.message_handler(commands=['start'])
@@ -13,12 +14,13 @@ def sorry(message):
 
 @bot.message_handler(commands=['create_task'])
 def new_task(message):
-    bot.send_message(message.chat.id, message)
-
-
-@bot.message_handler(content_types=['text'])
-def repeat(message):
-    bot.send_message(message.chat.id, message.text)
+    if message.chat.type == 'group':
+        tasks.append(' '.join(message.text.split()[1:]))
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(telebot.types.KeyboardButton('Я!'))
+        print(message)
+        bot.send_message(message.chat.id, ' '.join(message.text.split()[1:]), reply_markup=keyboard)
+    print(str(tasks))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
