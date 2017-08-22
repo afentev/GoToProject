d5d4d04a28ed56023665c4850d26d69ec295828f
afentev <kirill.afentyev@yandex.ru>
2017-08-22 19:53:04 +0300

    final changes

--- bot.py
+++ bot.py
@@ -27,18 +27,33 @@ def response_inline(call):
         if call.data == 'create_task':
             users.append(call.from_user.id)
             bot.send_message(call.from_user.id,
-                             'Отправьте сообщения следующего формата:\nЗаголовок задания\nКоличество требующихся исполнителей\nДата сдачи')
+                             'Отправьте сообщение следующего формата:\nЗаголовок задания (строка)\nКоличество требующихся исполнителей(натуральное число)\nДата сдачи(ММ.ДД ЧЧ:ММ)')
         for index, task in enumerate(tasks):
-            if call.data == task[1] + 'delete':
-                bot.send_message(call.from_user.id, 'Задание "{}" успешно удалено'.format(task[1]))
-                for id_ in tasks[tasks.index(task)][-1]:
+            if call.data == task['text'] + 'delete':
+                bot.send_message(call.from_user.id, 'Задание "{}" успешно удалено'.format(task['text']))
+                for id_ in task['users_accept']:
                     bot.send_message(id_,
-                                     'Задание "{}", на выполнение которого вы подписались, было удалено'.format(task[1]))
-                bot.delete_message(*task[-2])
+                                     'Задание "{}", на выполнение которого вы подписались, было удалено'.format(task['text']))
+                bot.delete_message(call.message.chat.id, call.message.message_id)
+                bot.send_message(chat_id, 'Задание "{}" было удалено'.format(task['text']))
                 del tasks[index]
-            elif call.data == task[1] + 'take':
-                tasks[index][-1].append(call.from_user.id)
-                bot.send_message(call.from_user.id, 'Ты взялся за выполнение задания "{}"'.format(task[1]))
+                break
+            elif call.data == task['text'] + 'take':
+                tasks[index]['count'] -= 1
+                tasks[index]['users_accept'].append('{} {}'.format(call.from_user.first_name, call.from_user.last_name))
+                bot.send_message(call.from_user.id, 'Ты взялся за выполнение задания "{}"'.format(task['text']))
+                if tasks[index]['count'] == 0:
+                    bot.send_message(task['chat.id'],
+                                     'Задание "{}" закрыто, так как набралось нужное количество участников'.format(task['text']))
+                    del tasks[index]
+                break
+            elif call.data == task['text'] + 'list':
+                if task['users_accept']:
+                    bot.send_message(call.from_user.id, text=', '.join(task['users_accept']))
+                else:
+                    bot.send_message(call.from_user.id, text='За выполнение этого задания еще никто не взялся')
+                break
+
 
 
 @bot.message_handler(content_types=['text'])
@@ -46,31 +61,45 @@ def handler(message):
     if message.text == '/delete':
         keyboard = telebot.types.InlineKeyboardMarkup()
         flag = False
-        print(tasks)
         for element in tasks:
-            if '{} {}'.format(message.from_user.first_name, message.from_user.last_name) == element[0]:
-                keyboard.add(telebot.types.InlineKeyboardButton(element[1], callback_data=element[1] + 'delete'))
+            if '{} {}'.format(message.from_user.first_name, message.from_user.last_name) == element['sender_name']:
+                keyboard.add(telebot.types.InlineKeyboardButton(element['text'], callback_data=element['text'] + 'delete'))
                 flag = True
         if flag:
             bot.send_message(message.chat.id, 'Выберите задание для удаления', reply_markup=keyboard)
-    if message.from_user.id in users:
+    elif message.from_user.id in users:
         if message.text.count('\n') == 2:
-            information = ['{} {}'.format(message.from_user.first_name, message.from_user.last_name),
-                           [message.chat.id, message.message_id]]
-            information.extend(deepcopy(message.text.split('\n')))
+            information = {'sender_name': '{} {}'.format(message.from_user.first_name, message.from_user.last_name)}
+            information['text'] = message.text.split('\n')[0]
+            information['count'] = message.text.split('\n')[1]
+            information['date'] = message.text.split('\n')[2]
+            information['chat.id'] = message.chat.id
             users.remove(message.from_user.id)
             chat_notification(information)
 
 
 def chat_notification(information):
-    user, text, count, date = information
-    string = '{} создал задание "{}" для {} человек! Задание активно до {}'.format(user, text, count, date)
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(telebot.types.InlineKeyboardButton(text='Я берусь', callback_data=text + 'take'))
-    keyboard.add(telebot.types.InlineKeyboardButton(text='Список взявшихся', callback_data=text + 'list'))
-    bot.send_message(chat_id=chat_id, text=string, reply_markup=keyboard)
-    information.append([])
-    tasks.append(information)
+    user, text, count, date, chat_id_ = list(information.values())
+    try:
+        count = int(count)
+        date = str(date)
+    except ValueError:
+        bot.send_message(chat_id=chat_id, text='Неверный формат введенных данных')
+        return
+
+    if isinstance(count, int) and isinstance(date, str) and date.count(' ') == 1 and date.split(' ')[0].count('.') == 1\
+            and date.split(' ')[1].count(':') == 1:
+        string = '{} создал задание "{}" для {} человек! Задание активно до {}'.format(user, text, count, date)
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.add(telebot.types.InlineKeyboardButton(text='Я берусь', callback_data=text + 'take'))
+        keyboard.add(telebot.types.InlineKeyboardButton(text='Список взявшихся', callback_data=text + 'list'))
+        bot.send_message(chat_id=chat_id, text=string, reply_markup=keyboard)
+        information['users_accept'] = []
+        tasks.append(information)
+        information['count'] = int(information['count'])
+    else:
+        bot.send_message(chat_id=chat_id_, text='Неверный формат введенных данных')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+
